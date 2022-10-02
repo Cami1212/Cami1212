f6af424c6ece4d8feb29fec54e58c49418e74691
Cami1212 <111406620+Cami1212@users.noreply.github.com>
2022-10-02 04:54:14 +0000

    Update Nodo.h

--- Nodo.h
+++ Nodo.h
@@ -1,1 +1,24 @@
+#include <string>
+#ifndef Nodo_h
+#define Nodo_h
+using namespace std;
+#include "Usuario.h"
+
+class Nodo
+{
+  private:
+    Usuario usuario;
+    Nodo *siguienteNodo;
+    
+    
+  public:
+    Nodo(Usuario u);
+    void setUsuario(Usuario u);
+    Usuario getUsuario();
+    Nodo *getSiguiente();
+    void setSiguiente(Nodo *sig);
+    string toString();
+};
+
+#endif
 
