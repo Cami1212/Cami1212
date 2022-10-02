adde63e2ff715cfff60e8a9c9859c3e800bbe4c4
Cami1212 <111406620+Cami1212@users.noreply.github.com>
2022-10-02 04:44:18 +0000

    Update UsuarioRepositorio.cpp

--- UsuarioRepositorio.cpp
+++ UsuarioRepositorio.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+using namespace std;
+
+#include <string>
+#include<cstdlib>
+#include "Usuario.h"
+
+
+Usuario::Usuario(){
+  
+}
+Usuario::Usuario(string u, string p){
+  username=u;
+  password=p;
+}
+
+Usuario::~Usuario(){
+}
+string Usuario::getUsername(){
+  return username;
+}
+void Usuario::setUsername(string s){
+  username =s;
+}
+string Usuario::getPassword(){
+  return password;
+}
+void Usuario::setPassword(string p){
+  password =p;
+}
+string Usuario::toString(){
+  return username + " " + password;
+}
 
