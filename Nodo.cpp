0b1c9c4b8adb55f517030f466162c1b7e24eb231
Cami1212 <111406620+Cami1212@users.noreply.github.com>
2022-10-02 04:53:37 +0000

    Update Nodo.cpp

--- Nodo.cpp
+++ Nodo.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+using namespace std;
+#include <string>
+#include "Usuario.h"
+#include "Nodo.h"
+
+Nodo::Nodo(Usuario u){
+  usuario = u;
+  siguienteNodo = NULL;
+}
+
+void Nodo::setUsuario(Usuario u){
+  usuario = u;
+}
+Usuario Nodo::getUsuario(){
+  return usuario;
+}
+Nodo *Nodo::getSiguiente(){
+  return siguienteNodo;
+}
+void Nodo::setSiguiente(Nodo *sig){
+  siguienteNodo = sig;
+}
+string Nodo::toString(){
+  string data = usuario.toString();
+  
+  return data;
+}
 
