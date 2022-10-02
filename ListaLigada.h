c13ffcf249f47d047f1b9aa951d79e0685943fad
Cami1212 <111406620+Cami1212@users.noreply.github.com>
2022-10-02 04:52:43 +0000

    Update ListaLigada.h

--- ListaLigada.h
+++ ListaLigada.h
@@ -1,1 +1,27 @@
+#include <string>
+#ifndef ListaLigada_h
+#define ListaLigada_h
+using namespace std;
+#include "Nodo.h"
+#include "Usuario.h"
 
+class ListaLigada{
+
+  private:
+    Nodo *primerNodo;
+    Nodo *buscarNodoPorUsuario(Usuario u);
+    void *borrarNodo(Nodo *n);
+
+
+  public:
+    ListaLigada();
+    Nodo *getPrimerNodo();
+    bool esVacia();
+    void insertar(Usuario u);
+    void imprimirLista();
+    Usuario buscarUsuario(Usuario u);
+    void borrarUsuario(Usuario u);
+
+};
+
+#endif
