182b6a173194d605d9aedd420697d76571d5aba8
Cami1212 <111406620+Cami1212@users.noreply.github.com>
2022-10-02 04:45:18 +0000

    Update UsuarioRepositorio.h

--- UsuarioRepositorio.h
+++ UsuarioRepositorio.h
@@ -1,1 +1,21 @@
+#define Usuario_h
+using namespace std;
+
+class Usuario
+{
+  private:
+    string username;
+    string password;
+    
+    
+  public:
+    Usuario();
+    Usuario(string u, string p);
+    ~Usuario();
+    string getUsername();
+    void setUsername(string u);
+    string getPassword();
+    void setPassword(string p);
+    string toString();
+};
 
